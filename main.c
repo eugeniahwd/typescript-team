70b67dfd28d63dc9d0c1088893669206c6110223
nadizzati <nadiaizzati.024@gmail.com>
2025-05-14 20:12:04 +0700

    fungsi luas & validasi input

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
+float calculate_area(float height, float width){
+    return height * width;
+}
+
+int validate_input(float value){
+    if (value <= 0){
+        printf("Invalid: Value must be positive!\n");
+        return 0;
+    }
+    return 1;
+}
+
 int main(){
     float height, width;
     printf("=== Rectangle Area Calculator ===\n");
