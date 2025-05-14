5c9eaccff7a0b5642208e325e2992c0ebdd5a829
eugeniahwd <eugeniahwda@gmail.com>
2025-05-14 16:48:27 +0700

    our project

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    float height, width;
+    printf("=== Rectangle Area Calculator ===\n");
+    printf("Enter the height: "); scanf("%f", &height);
+    printf("Enter the width: "); scanf("%f", &width);
+}
